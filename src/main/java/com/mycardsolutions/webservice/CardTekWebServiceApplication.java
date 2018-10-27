11fe3cd8d0fb8acde8316708f71350a00d8f0250
sarika-mane <sarika.mane24@gmail.com>
2018-10-27 18:09:00 +0530

    Removed unused

--- src/main/java/com/mycardsolutions/webservice/CardTekWebServiceApplication.java
+++ src/main/java/com/mycardsolutions/webservice/CardTekWebServiceApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import com.mycardsolutions.webservice.controller.SoapServiceController;
-
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.mycardsolutions.webservice"})
 public class CardTekWebServiceApplication {
